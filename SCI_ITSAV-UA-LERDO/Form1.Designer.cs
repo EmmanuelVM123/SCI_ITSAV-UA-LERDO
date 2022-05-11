c91f0a20b9ff256edd854e2c401fdc9653d5e8f7
Emman <Emman@EVM>
2022-05-11 14:34:58 -0500

    Agregar archivos de proyecto.

---
+++ SCI_ITSAV-UA-LERDO/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿
+namespace SCI_ITSAV_UA_LERDO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelMenuLateral = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.subMenu = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnInventarioAreas = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.panelAccion = new System.Windows.Forms.Panel();
+            this.panelFomularioHijo = new System.Windows.Forms.Panel();
+            this.PanelMenuLateral.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.subMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelMenuLateral
+            // 
+            this.PanelMenuLateral.AutoScroll = true;
+            this.PanelMenuLateral.BackColor = System.Drawing.Color.Salmon;
+            this.PanelMenuLateral.Controls.Add(this.panel2);
+            this.PanelMenuLateral.Controls.Add(this.button8);
+            this.PanelMenuLateral.Controls.Add(this.panel1);
+            this.PanelMenuLateral.Controls.Add(this.button1);
+            this.PanelMenuLateral.Controls.Add(this.subMenu);
+            this.PanelMenuLateral.Controls.Add(this.btnInventarioAreas);
+            this.PanelMenuLateral.Controls.Add(this.panelLogo);
+            this.PanelMenuLateral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelMenuLateral.Location = new System.Drawing.Point(0, 0);
+            this.PanelMenuLateral.Name = "PanelMenuLateral";
+            this.PanelMenuLateral.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.PanelMenuLateral.Size = new System.Drawing.Size(250, 561);
+            this.PanelMenuLateral.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightCoral;
+            this.panel2.Controls.Add(this.button9);
+            this.panel2.Controls.Add(this.button10);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 605);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(233, 97);
+            this.panel2.TabIndex = 6;
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(0, 45);
+            this.button9.Name = "button9";
+            this.button9.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button9.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button9.Size = new System.Drawing.Size(233, 45);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button10.FlatAppearance.BorderSize = 0;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Location = new System.Drawing.Point(0, 0);
+            this.button10.Name = "button10";
+            this.button10.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button10.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button10.Size = new System.Drawing.Size(233, 45);
+            this.button10.TabIndex = 0;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(0, 560);
+            this.button8.Name = "button8";
+            this.button8.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button8.Size = new System.Drawing.Size(233, 45);
+            this.button8.TabIndex = 5;
+            this.button8.Text = "Bajas";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCoral;
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.button13);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 463);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(233, 97);
+            this.panel1.TabIndex = 4;
+            // 
+            // button12
+            // 
+            this.button12.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button12.FlatAppearance.BorderSize = 0;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.Location = new System.Drawing.Point(0, 45);
+            this.button12.Name = "button12";
+            this.button12.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button12.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button12.Size = new System.Drawing.Size(233, 45);
+            this.button12.TabIndex = 1;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button13.FlatAppearance.BorderSize = 0;
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button13.Location = new System.Drawing.Point(0, 0);
+            this.button13.Name = "button13";
+            this.button13.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button13.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button13.Size = new System.Drawing.Size(233, 45);
+            this.button13.TabIndex = 0;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(0, 418);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(233, 45);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Altas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // subMenu
+            // 
+            this.subMenu.BackColor = System.Drawing.Color.LightCoral;
+            this.subMenu.Controls.Add(this.button7);
+            this.subMenu.Controls.Add(this.button6);
+            this.subMenu.Controls.Add(this.button5);
+            this.subMenu.Controls.Add(this.button4);
+            this.subMenu.Controls.Add(this.button3);
+            this.subMenu.Controls.Add(this.button2);
+            this.subMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.subMenu.Location = new System.Drawing.Point(0, 145);
+            this.subMenu.Name = "subMenu";
+            this.subMenu.Size = new System.Drawing.Size(233, 273);
+            this.subMenu.TabIndex = 2;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(0, 225);
+            this.button7.Name = "button7";
+            this.button7.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button7.Size = new System.Drawing.Size(233, 45);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Archivo Muerto";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(0, 180);
+            this.button6.Name = "button6";
+            this.button6.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button6.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button6.Size = new System.Drawing.Size(233, 45);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Laboratorio de Cómputo";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(0, 135);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button5.Size = new System.Drawing.Size(233, 45);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Mantenimiento";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(0, 90);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button4.Size = new System.Drawing.Size(233, 45);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Intendencia";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(0, 45);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button3.Size = new System.Drawing.Size(233, 45);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Servicios Generales";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(233, 45);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Coordinación-Ingresos propios-Servicios Escolares";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnInventarioAreas
+            // 
+            this.btnInventarioAreas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInventarioAreas.FlatAppearance.BorderSize = 0;
+            this.btnInventarioAreas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInventarioAreas.Location = new System.Drawing.Point(0, 100);
+            this.btnInventarioAreas.Name = "btnInventarioAreas";
+            this.btnInventarioAreas.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnInventarioAreas.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnInventarioAreas.Size = new System.Drawing.Size(233, 45);
+            this.btnInventarioAreas.TabIndex = 1;
+            this.btnInventarioAreas.Text = "Inventario por áreas";
+            this.btnInventarioAreas.UseVisualStyleBackColor = true;
+            this.btnInventarioAreas.Click += new System.EventHandler(this.btnInventarioAreas_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(233, 100);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // panelAccion
+            // 
+            this.panelAccion.BackColor = System.Drawing.Color.MistyRose;
+            this.panelAccion.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelAccion.Location = new System.Drawing.Point(250, 461);
+            this.panelAccion.Name = "panelAccion";
+            this.panelAccion.Size = new System.Drawing.Size(684, 100);
+            this.panelAccion.TabIndex = 1;
+            // 
+            // panelFomularioHijo
+            // 
+            this.panelFomularioHijo.BackColor = System.Drawing.Color.LightSalmon;
+            this.panelFomularioHijo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelFomularioHijo.Location = new System.Drawing.Point(250, 0);
+            this.panelFomularioHijo.Name = "panelFomularioHijo";
+            this.panelFomularioHijo.Size = new System.Drawing.Size(684, 461);
+            this.panelFomularioHijo.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 561);
+            this.Controls.Add(this.panelFomularioHijo);
+            this.Controls.Add(this.panelAccion);
+            this.Controls.Add(this.PanelMenuLateral);
+            this.MinimumSize = new System.Drawing.Size(950, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.PanelMenuLateral.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.subMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelMenuLateral;
+        private System.Windows.Forms.Button btnInventarioAreas;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel subMenu;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelAccion;
+        private System.Windows.Forms.Panel panelFomularioHijo;
+    }
+}
+
---
+++ SCI_ITSAV-UA-LERDO/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCI_ITSAV_UA_LERDO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PersonalizarDiseño();
+        }
+        private void PersonalizarDiseño()
+        {
+            subMenu.Visible = false;
+            panel1.Visible = false;
+            panel2.Visible = false;
+             
+        }
+
+        private void OcultarSubMenus()
+        {
+            if (subMenu.Visible == false)
+                subMenu.Visible = false;
+            if (panel1.Visible == false)
+                panel1.Visible = false;
+            if (panel2.Visible == false)
+                panel2.Visible=false;                        
+        }
+        
+        private void MostrarSubMenu (Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                OcultarSubMenus();
+                subMenu.Visible = true;  
+            }
+            else 
+                subMenu.Visible = false;
+
+        }
+
+        private void btnInventarioAreas_Click(object sender, EventArgs e)
+        {
+            MostrarSubMenu(subMenu);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AbrirFormularioHijo(new CoordinaciónIngresosPropiosServiciosEscolares());
+
+            OcultarSubMenus();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MostrarSubMenu(panel1);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MostrarSubMenu(panel2);
+        }
+        private Form formularioActivo = null;
+        private void AbrirFormularioHijo(Form formularioHijo)
+        {
+            if (formularioActivo != null)
+                formularioActivo.Close();
+            formularioActivo = formularioHijo;
+            formularioHijo.TopLevel = false;
+            formularioHijo.FormBorderStyle = FormBorderStyle.None;
+            formularioHijo.Dock = DockStyle.Fill;
+            panelFomularioHijo.Controls.Add(formularioHijo);
+            panelFomularioHijo.Tag = formularioHijo;
+            formularioHijo.BringToFront();
+            formularioHijo.Show();
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AbrirFormularioHijo(new ServiciosGenerales());
+
+            OcultarSubMenus();
+        }
+    }
+}
